38c553ef3c8f5c9a7b8ed28062737273a072c650
Chris Johnson <chris@johnsonch.com>
2017-07-31 21:21:26 -0500

    first user spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    
+    email { Faker::Internet.email }
+    password 'P@ssw0rd!'
+    password_confirmation 'P@ssw0rd!'
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "creating user" do
+    it 'is valid with factory' do
+      user = FactoryGirl.build(:user)
+      expect(user).to be_valid
+    end
+  end
 end
